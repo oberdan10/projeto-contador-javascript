8975f776a1a9be8ad5d1b4634c713983c055054b
Oberdan Rocha <oberdan.rbsantos@gmail.com>
2022-06-22 21:49:22 -0300

    Projeto sintaxe e operadores

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # projeto-contador-javascript
 * Projeto Dio Contador JavaScript
 * Projeto To-Do List Javascript
+* Projeto Sintaxe e Operadores Javascript
--- SintaxeEOperadores/script.js
+++ SintaxeEOperadores/script.js
@@ -11,8 +11,6 @@ function Comparadordenumeros(numero1, numero2){
     condicaomenorque20 = (soma_numeros<20) ? "menor que" : "maior(ou igual) a";
 
     console.log(`Os números ${numero1} e ${numero2} ${numeros_iguais} iguais. Sua soma é ${soma_numeros}, que é ${condicaomaiorque10} 10 e ${condicaomenorque20} 20.`);
-
 }
-
 
 Comparadordenumeros(5, 5);
